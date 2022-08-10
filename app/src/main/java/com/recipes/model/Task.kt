c752232982667dc626554658b8ce5c354bd22927
Adam Visegradi <adam.visegradi@ericsson.com>
2022-08-10 21:18:23 +0200

    Fix Task references; take 1

--- app/src/main/java/com/recipes/model/Task.kt
+++ app/src/main/java/com/recipes/model/Task.kt
@@ -1,10 +1,16 @@
 package com.recipes.model
 
+typealias TaskId = String
+
+internal fun Collection<Task>.hasUniqueIds(): Boolean =
+    this.groupBy { it.id }.size == this.size
+
 class Task(
     //image
     //timer -> task mapping
+    val id: TaskId,
     val description: String,
-    val ingredients: Array<Ingredient> = emptyArray(),
-    val nextTask: Task? = null, // When null, go to next in array
+    val ingredients: List<Ingredient> = emptyList(),
+    val nextTask: TaskId? = null, // When null, go to next in array
 )
 
